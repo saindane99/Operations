90ce94536391aa161ed7669fd670a479eface29a
Sanginfo-Shailesh <shailesh.saindane@sanginfo.com>
2018-08-30 16:14:07 +0530

    files and gradles added

--- app/src/main/java/com/mathoperations/MainActivity.java
+++ app/src/main/java/com/mathoperations/MainActivity.java
@@ -3,11 +3,15 @@ package com.mathoperations;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mathoperations.arithmetics.MyMath;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyMath.Plus(10,10);
     }
 }
